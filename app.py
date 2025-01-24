d7371fdf39d00e22a51b6582bd6beba39d8accc7
vrm2310 <vyomrm@gmail.com>
2025-01-25 02:22:22 +0530

    blah

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from functools import wraps
 from flask import render_template, request, redirect, url_for, session 
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
@@ -11,6 +12,7 @@ app = Flask(__name__, template_folder='birdwise/templates', static_folder='birdw
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///birdwise.db'
 app.config['SECRET_KEY'] = '8b435b3f121b6273e153d976'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
