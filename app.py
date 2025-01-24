10420d10b88213328554db8b0faa5fc3c894c527
vrm2310 <63455502+vrm2310@users.noreply.github.com>
2025-01-24 18:31:57 +0530

    Add files via upload
    flask app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+# to create authentication
+from functools import wraps
+from flask import render_template, request, redirect, url_for, session 
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '8b435b3f121b6273e153d976'
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+
+with app.app_context():
+    pass
