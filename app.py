935b0be1ce34960628f8b46e936fdada9056283c
vrm2310 <vyomrm@gmail.com>
2025-01-24 20:53:35 +0530

    erhedshje

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='birdwise/templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///birdwise.db'
 app.config['SECRET_KEY'] = '8b435b3f121b6273e153d976'
 db = SQLAlchemy(app)
@@ -15,6 +15,8 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
+
+from birdwise import routes
 
 with app.app_context():
     pass
